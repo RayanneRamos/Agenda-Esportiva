c88d80976d754245b452660a05b21ad7bca3a720
Rayanne Ramos <rayanneramosoficial@gmail.com>
2021-04-26 15:03:38 -0300

    Adicionando as informações da agenda

---
+++ src/common/fixtures/agenda.ts
@@ -0,0 +1,14 @@
+export default {
+  modalidade: "Futebol",
+  etapa: "Rodada 1",
+  data: "2021-04-20",
+  hora: "21:30",
+  mandante: {
+    nome: "Vélez Sarsfield",
+    urlImagem: "https://s.glbimg.com/es/sde/f/organizacoes/2011/01/26/velez_sarsfield.png"
+  },
+  visitante: {
+    nome: "Flamengo",
+    urlImagem: "https://s.glbimg.com/es/sde/f/organizacoes/2018/04/10/Flamengo-2018.svg"
+  }
+}
