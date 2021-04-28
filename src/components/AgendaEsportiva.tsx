c0a3847a6f21214e748f233fb01696852ef84464
Rayanne Ramos <rayanneramosoficial@gmail.com>
2021-04-28 19:41:53 -0300

    Modificando o componente agenda esportiva

--- src/components/AgendaEsportiva.tsx
+++ src/components/AgendaEsportiva.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Championship, MatchBox, Header, HeaderItem, MatchPlay, Teams, Badge, Team, PlayInfo } from './AgendaEsportiva.style';
+import { Championship, AgendaWrapper, MatchBox, Header, HeaderItem, MatchPlay, Teams, Badge, Team, PlayInfo } from './AgendaEsportiva.style';
 import { Campeonato, Jogo } from '../common/types/agenda'; 
 
 type Props = {
@@ -9,38 +9,30 @@ type Props = {
 const AgendaEsportiva: React.FC<Props> = ({ championshipEdition }: Props) => {
   return (
     <>
-      <Championship>{championshipEdition.campeonato}</Championship>
-      <MatchBox>
-          <Header>
-            {championshipEdition.jogos.map((item: Jogo, key: number) => {
-              return (
-                <div key={key}>
-                  <HeaderItem>{item.modalidade}</HeaderItem>
-                  <HeaderItem>{item.hora}</HeaderItem>
-                </div>
-              )
-            })}
-          </Header>
-          <MatchPlay>
-              {championshipEdition.jogos.map((item: Jogo, key: number) => {
-                return (
-                  <div key={key}>
-                    <Teams>
-                      <Badge src={item.mandante.urlImagem} />
-                      <Team>{item.mandante.nome}</Team>
-                    </Teams>
-                    <Teams>
-                      <Badge src={item.visitante.urlImagem} />
-                      <Team>{item.visitante.nome}</Team>
-                    </Teams>
-                  </div>
-                )
-              })} 
-          </MatchPlay>
-          {championshipEdition.jogos.map((item: Jogo, key: number) => {
-            return <PlayInfo key={key}>{item.etapa}</PlayInfo>
-          })}
-      </MatchBox>
+    <Championship>{championshipEdition.campeonato}</Championship>
+      {championshipEdition.jogos.map((item: Jogo, key: number) => {
+        return (
+          <AgendaWrapper>
+            <MatchBox key={`matchBox-${key}`}> 
+              <Header>
+                <HeaderItem>{item.modalidade}</HeaderItem>
+                <HeaderItem>{item.hora}</HeaderItem>
+              </Header>
+            <MatchPlay>
+              <Teams>
+                <Badge src={item.mandante.urlImagem} />
+                <Team>{item.mandante.nome}</Team>
+              </Teams>
+              <Teams>
+                <Badge src={item.visitante.urlImagem} />
+                <Team>{item.visitante.nome}</Team>
+              </Teams>
+              <PlayInfo key={key}>{item.etapa}</PlayInfo>
+            </MatchPlay>
+            </MatchBox>
+          </AgendaWrapper>
+        )
+      })}
     </>
   );
 }
