c5e9abb12c6d0e58eeaf4c2d053ab2d75024f37e
Rayanne Ramos <rayanneramosoficial@gmail.com>
2021-04-29 11:10:04 -0300

    Adicionando novos trechos de códigos no componente agenda esportiva

--- src/components/AgendaEsportiva.tsx
+++ src/components/AgendaEsportiva.tsx
@@ -9,28 +9,30 @@ type Props = {
 const AgendaEsportiva: React.FC<Props> = ({ championshipEdition }: Props) => {
   return (
     <>
-    <Championship>{championshipEdition.campeonato}</Championship>
-      {championshipEdition.jogos.map((item: Jogo, key: number) => {
-        return (
-          <AgendaWrapper>
-            <MatchBox key={`matchBox-${key}`}> 
-              <Header>
-                <HeaderItem>{item.modalidade}</HeaderItem>
-                <HeaderItem>{item.hora}</HeaderItem>
-              </Header>
-            <MatchPlay>
-              <Teams>
-                <Badge src={item.mandante.urlImagem} />
-                <Team>{item.mandante.nome}</Team>
-              </Teams>
-              <Teams>
-                <Badge src={item.visitante.urlImagem} />
-                <Team>{item.visitante.nome}</Team>
-              </Teams>
-              <PlayInfo key={key}>{item.etapa}</PlayInfo>
-            </MatchPlay>
-            </MatchBox>
-          </AgendaWrapper>
+      <div>
+        <Championship>{championshipEdition.campeonato}</Championship>
+      </div>
+        {championshipEdition.jogos.map((item: Jogo, key: number) => {
+          return (
+            <AgendaWrapper>
+              <MatchBox key={`matchBox-${key}`}> 
+                <Header>
+                  <HeaderItem>{item.modalidade}</HeaderItem>
+                  <HeaderItem>{item.hora}</HeaderItem>
+                </Header>
+              <MatchPlay>
+                <Teams>
+                  <Badge src={item.mandante.urlImagem} />
+                  <Team>{item.mandante.nome}</Team>
+                </Teams>
+                <Teams>
+                  <Badge src={item.visitante.urlImagem} />
+                  <Team>{item.visitante.nome}</Team>
+                </Teams>
+                <PlayInfo key={key}>{item.etapa}</PlayInfo>
+              </MatchPlay>
+              </MatchBox>
+            </AgendaWrapper>
         )
       })}
     </>
