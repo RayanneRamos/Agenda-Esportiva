be0bb4ef019f9486ba678216ca195d90c64745f4
Rayanne Ramos <rayanneramosoficial@gmail.com>
2021-05-03 14:13:31 -0300

    Modificando o arquivo de fixtures

--- src/common/fixtures/agenda.ts
+++ src/common/fixtures/agenda.ts
@@ -1,89 +1,145 @@
 export default {
-        campeonato: "Taça Libertadores",
-        jogos: [
-          {
-            modalidade: "Futebol",
-            etapa: "Rodada 1",
-            data: "2021-04-20",
-            hora: "19:15",
-            mandante: {
-              nome: "Argentino Juniors",
-              urlImagem: "https://s.glbimg.com/es/sde/f/organizacoes/2011/01/26/argentino_juniors.png"
-            },
-            visitante: {
-              nome: "Nacional-URU",
-              urlImagem: "https://s.glbimg.com/es/sde/f/organizacoes/2019/02/18/nacional-uru-svg-13086.svg"
-            }
+  campeonatos: [
+    {
+      campeonato: "Taça Libertadores",
+      jogos: [
+        {
+          modalidade: "Futebol",
+          etapa: "Rodada 1",
+          data: "2021-04-20",
+          hora: "19:15",
+          mandante: {
+            nome: "Argentino Juniors",
+            urlImagem: "https://s.glbimg.com/es/sde/f/organizacoes/2011/01/26/argentino_juniors.png"
           },
-          {
-            modalidade: "Futebol",
-            etapa: "Rodada 1",
-            data: "2021-04-20",
-            hora: "19:15",
-            mandante: {
-              nome: "Always Ready",
-              urlImagem: "https://s.glbimg.com/es/sde/f/organizacoes/2019/12/26/always-ready-23152.svg"
-            },
-            visitante: {
-              nome: "Internacional",
-              urlImagem: "https://s.glbimg.com/es/sde/f/organizacoes/2018/03/11/internacional.svg"
-            }
-          },
-          {
-            modalidade: "Futebol",
-            etapa: "Rodada 1",
-            data: "2021-04-20",
-            hora: "19:15",
-            mandante: {
-              nome: "Deportivo Táchira",
-              urlImagem: "https://s.glbimg.com/es/sde/f/organizacoes/2011/01/26/deportivo_tachira.png"
-            },
-            visitante: {
-              nome: "Olimpia",
-              urlImagem: "https://s.glbimg.com/es/sde/f/organizacoes/2019/02/19/olimpia-svg-13097.svg"
-            }
-          },
-          {
-            modalidade: "Futebol",
-            etapa: "Rodada 1",
-            data: "2021-04-20",
-            hora: "19:15",
-            mandante: {
-              nome: "Santos",
-              urlImagem: "https://s.glbimg.com/es/sde/f/organizacoes/2018/03/12/santos.svg"
-            },
-            visitante: {
-              nome: "Barcelona de Guayaquil",
-              urlImagem: "https://s.glbimg.com/es/sde/f/equipes/2017/03/14/BarcelonaGuayaquil-30.png"
-            }
-          },
-          {
-            modalidade: "Futebol",
-            etapa: "Rodada 1",
-            data: "2021-04-20",
-            hora: "21:30",
-            mandante: {
-              nome: "Vélez Sarsfield",
-              urlImagem: "https://s.glbimg.com/es/sde/f/organizacoes/2011/01/26/velez_sarsfield.png"
-            },
-            visitante: {
-              nome: "Flamengo",
-              urlImagem: "https://s.glbimg.com/es/sde/f/organizacoes/2018/04/10/Flamengo-2018.svg"
-            }
-          },
-          {
-            modalidade: "Futebol",
-            etapa: "Rodada 1",
-            data: "2021-04-20",
-            hora: "21:30",
-            mandante: {
-              nome: "Sporting Cristal",
-              urlImagem: "https://s.glbimg.com/es/sde/f/organizacoes/2019/02/19/sporting-cristal-svg-13103.svg"
-            },
-            visitante: {
-              nome: "São Paulo",
-              urlImagem: "https://s.glbimg.com/es/sde/f/organizacoes/2018/03/11/sao-paulo.svg"
-            }
+          visitante: {
+            nome: "Nacional-URU",
+            urlImagem: "https://s.glbimg.com/es/sde/f/organizacoes/2019/02/18/nacional-uru-svg-13086.svg"
           }
-        ]
-  }
+        },
+        {
+          modalidade: "Futebol",
+          etapa: "Rodada 1",
+          data: "2021-04-20",
+          hora: "19:15",
+          mandante: {
+            nome: "Always Ready",
+            urlImagem: "https://s.glbimg.com/es/sde/f/organizacoes/2019/12/26/always-ready-23152.svg"
+          },
+          visitante: {
+            nome: "Internacional",
+            urlImagem: "https://s.glbimg.com/es/sde/f/organizacoes/2018/03/11/internacional.svg"
+          }
+        },
+        {
+          modalidade: "Futebol",
+          etapa: "Rodada 1",
+          data: "2021-04-20",
+          hora: "19:15",
+          mandante: {
+            nome: "Deportivo Táchira",
+            urlImagem: "https://s.glbimg.com/es/sde/f/organizacoes/2011/01/26/deportivo_tachira.png"
+          },
+          visitante: {
+            nome: "Olimpia",
+            urlImagem: "https://s.glbimg.com/es/sde/f/organizacoes/2019/02/19/olimpia-svg-13097.svg"
+          }
+        },
+        {
+          modalidade: "Futebol",
+          etapa: "Rodada 1",
+          data: "2021-04-20",
+          hora: "19:15",
+          mandante: {
+            nome: "Santos",
+            urlImagem: "https://s.glbimg.com/es/sde/f/organizacoes/2018/03/12/santos.svg"
+          },
+          visitante: {
+            nome: "Barcelona de Guayaquil",
+            urlImagem: "https://s.glbimg.com/es/sde/f/equipes/2017/03/14/BarcelonaGuayaquil-30.png"
+          }
+        },
+        {
+          modalidade: "Futebol",
+          etapa: "Rodada 1",
+          data: "2021-04-20",
+          hora: "21:30",
+          mandante: {
+            nome: "Vélez Sarsfield",
+            urlImagem: "https://s.glbimg.com/es/sde/f/organizacoes/2011/01/26/velez_sarsfield.png"
+          },
+          visitante: {
+            nome: "Flamengo",
+            urlImagem: "https://s.glbimg.com/es/sde/f/organizacoes/2018/04/10/Flamengo-2018.svg"
+          }
+        },
+        {
+          modalidade: "Futebol",
+          etapa: "Rodada 1",
+          data: "2021-04-20",
+          hora: "21:30",
+          mandante: {
+            nome: "Sporting Cristal",
+            urlImagem: "https://s.glbimg.com/es/sde/f/organizacoes/2019/02/19/sporting-cristal-svg-13103.svg"
+          },
+          visitante: {
+            nome: "São Paulo",
+            urlImagem: "https://s.glbimg.com/es/sde/f/organizacoes/2018/03/11/sao-paulo.svg"
+          }
+        }
+      ]
+    },
+    {
+      campeonato: "Campeonato Paulista",
+      jogos: [
+        {
+          modalidade: "Futebol",
+          etapa: "Rodada 9",
+          data: "2021-04-20",
+          hora: "20:00",
+          mandante: {
+            nome: "Novorizontino",
+            urlImagem: "https://s.glbimg.com/es/sde/f/organizacoes/2019/01/08/Novohorizontino.svg"
+          },
+          visitante: {
+            nome: "Santo André",
+            urlImagem: "https://s.glbimg.com/es/sde/f/organizacoes/2019/05/13/SantoAndreSVG.svg"
+          },
+        },
+        {
+          modalidade: "Futebol",
+          etapa: "Rodada 5",
+          data: "2021-04-20",
+          hora: "21:30",
+          mandante: {
+            nome: "Ferroviária",
+            urlImagem: "https://s.glbimg.com/es/sde/f/organizacoes/2019/01/08/Ferroviaria_Araraquara.svg"
+          },
+          visitante: {
+            nome: "Guarani",
+            urlImagem: "https://s.glbimg.com/es/sde/f/organizacoes/2018/03/11/guarani.svg"
+          }
+        }
+      ]
+    },
+    {
+      campeonato: "Campeonato Gaúcho",
+      jogos: [
+        {
+          modalidade: "Futebol",
+          etapa: "Rodada 10",
+          data: "2021-04-20",
+          hora: "20:00",
+          mandante: {
+            nome: "Caxias",
+            urlImagem: "https://s.glbimg.com/es/sde/f/organizacoes/2019/01/05/svg-caxias.svg"
+          },
+          visitante: {
+            nome: "https://s.glbimg.com/es/sde/f/organizacoes/2019/05/13/YpirangaRSSVG.svg",
+            urlImagem: "https://s.glbimg.com/es/sde/f/organizacoes/2019/05/13/SantoAndreSVG.svg"
+          }
+        }
+      ]
+    }
+  ]
+}
