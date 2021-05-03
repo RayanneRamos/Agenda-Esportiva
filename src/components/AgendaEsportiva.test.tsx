66b52b53b271dbbcdf77780c6d307be19621cc6b
Rayanne Ramos <rayanneramosoficial@gmail.com>
2021-05-03 17:02:18 -0300

    Os testes feitos no componente da Agenda Esportiva estão todos funcionando

--- src/components/AgendaEsportiva.test.tsx
+++ src/components/AgendaEsportiva.test.tsx
@@ -16,47 +16,47 @@ describe('AgendaEsportiva', () => {
 
     it('should contain MatchBox', () => {
       const matchBox = agendaEsportiva.find(MatchBox);
-      expect(matchBox.length).toBe(6);
+      expect(matchBox.length).toBe(9);
     });
 
     it('should contain Header', () => {
       const header = agendaEsportiva.find(Header);
-      expect(header.length).toBe(6);
+      expect(header.length).toBe(9);
     });
 
     it('should contain HeaderItem', () => {
       const headerItem = agendaEsportiva.find(HeaderItem);
-      expect(headerItem.length).toBe(12);
+      expect(headerItem.length).toBe(18);
     });
 
     it('should contain AgendaWrapper', () => {
       const agendaWrapper = agendaEsportiva.find(AgendaWrapper);
-      expect(agendaWrapper.length).toBe(6);
+      expect(agendaWrapper.length).toBe(9);
     })
 
     it('should contain MatchPlay', () => {
       const matchPlay = agendaEsportiva.find(MatchPlay);
-      expect(matchPlay.length).toBe(6);
+      expect(matchPlay.length).toBe(9);
     });
     
     it('should contain Teams', () => {
       const teams = agendaEsportiva.find(Teams);
-      expect(teams.length).toBe(12);
+      expect(teams.length).toBe(18);
     })
 
     it('should contain Badge', () => {
       const badge = agendaEsportiva.find(Badge);
-      expect(badge.length).toBe(12);
+      expect(badge.length).toBe(18);
     });
 
     it('should contain Team', () => {
       const team = agendaEsportiva.find(Team);
-      expect(team.length).toBe(12);
+      expect(team.length).toBe(18);
     });
 
     it('should contain PlayInfo', () => {
       const playInfo = agendaEsportiva.find(PlayInfo);
-      expect(playInfo.length).toBe(6);
+      expect(playInfo.length).toBe(9);
     });
   });
 });
