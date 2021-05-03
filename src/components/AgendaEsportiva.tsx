b217a0a6b3b0512457fbb03220d8b07c6464dd3f
Rayanne Ramos <rayanneramosoficial@gmail.com>
2021-05-03 14:15:33 -0300

    Componente Agenda Esportiva

--- src/components/AgendaEsportiva.tsx
+++ src/components/AgendaEsportiva.tsx
@@ -1,40 +1,44 @@
 import React from 'react';
 import { Championship, AgendaWrapper, MatchBox, Header, HeaderItem, MatchPlay, Teams, Badge, Team, PlayInfo } from './AgendaEsportiva.style';
-import { Campeonato, Jogo } from '../common/types/agenda'; 
+import { Campeonatos, Campeonato, Jogo } from '../common/types/agenda'; 
 
 type Props = {
-  championshipEdition: Campeonato
+  championshipEdition: Campeonatos
 }
 
 const AgendaEsportiva: React.FC<Props> = ({ championshipEdition }: Props) => {
   return (
     <>
-      <div>
-        <Championship>{championshipEdition.campeonato}</Championship>
-      </div>
-        {championshipEdition.jogos.map((item: Jogo, key: number) => {
-          return (
+      {championshipEdition.campeonatos.map((item: Campeonato, key: number) => {
+        return (
+          <div key={`championship-${key}`}>
+            <Championship>{item.campeonato}</Championship>
+          </div>
+        )
+      })}
+         {championshipEdition.jogos.map((item: Jogo, key: number) => {
+           return (
             <AgendaWrapper>
-              <MatchBox key={`matchBox-${key}`}> 
+              <MatchBox> 
                 <Header>
                   <HeaderItem>{item.modalidade}</HeaderItem>
                   <HeaderItem>{item.hora}</HeaderItem>
                 </Header>
-              <MatchPlay>
-                <Teams>
-                  <Badge src={item.mandante.urlImagem} />
-                  <Team>{item.mandante.nome}</Team>
-                </Teams>
-                <Teams>
-                  <Badge src={item.visitante.urlImagem} />
-                  <Team>{item.visitante.nome}</Team>
-                </Teams>
-                <PlayInfo key={key}>{item.etapa}</PlayInfo>
-              </MatchPlay>
+                <MatchPlay>
+                  <Teams>
+                    <Badge src={item.mandante.urlImagem} />
+                    <Team>{item.mandante.nome}</Team>
+                  </Teams>
+                  <Teams>
+                    <Badge src={item.visitante.urlImagem} />
+                    <Team>{item.visitante.nome}</Team>
+                  </Teams>
+                  <PlayInfo>{item.etapa}</PlayInfo>
+                </MatchPlay>
               </MatchBox>
             </AgendaWrapper>
-        )
-      })}
+          )
+         })} 
     </>
   );
 }
