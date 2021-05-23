5cbae71011cba9007442b512b3e28c868f8485be
Rayanne Ramos <rayanneramosoficial@gmail.com>
2021-05-23 00:13:50 -0300

    Retirando o Championship Content e o Championship

--- src/components/AgendaEsportiva.style.tsx
+++ src/components/AgendaEsportiva.style.tsx
@@ -1,18 +1,18 @@
 import styled from 'styled-components';
 
-export const ChampionshipContent = styled.div `
-  padding-top: 15px;
-  padding-bottom: 20px; 
-`;
+// export const ChampionshipContent = styled.div `
+//   padding-top: 15px;
+//   padding-bottom: 20px; 
+// `;
 
-export const Championship = styled.span `
-  padding-bottom: 16px;
-  color: #515151;
-  font-size: 16px;
-  font-weight: 600;
-  line-height: 1.5;
-  font-family: "Open Sans", sans-serif;
-`;
+// export const Championship = styled.span `
+//   padding-bottom: 16px;
+//   color: #515151;
+//   font-size: 16px;
+//   font-weight: 600;
+//   line-height: 1.5;
+//   font-family: "Open Sans", sans-serif;
+// `;
 
 export const HeaderPrincipal = styled.div `
   display: flex;
