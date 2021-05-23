fe732584e4660ec3c120db7775ffe4c300b9c73b
Rayanne Ramos <rayanneramosoficial@gmail.com>
2021-05-23 00:13:08 -0300

    Importando o championship no App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import AgendaEsportiva from './components/AgendaEsportiva';
 import championshipEditionMock from './common/fixtures/agenda';
-import { Campeonatos, Dados } from './common/types/agenda';
+import { Campeonatos, Campeonato, Dados } from './common/types/agenda';
+import campeonato from './components/ChampionshipWrapper/ChampionshipWrapper';
 
 const App = () => {
   return (
-    <AgendaEsportiva championshipEdition={championshipEditionMock as Campeonatos } info={championshipEditionMock as Dados} />
+    <AgendaEsportiva 
+      championshipEdition={championshipEditionMock as Campeonatos }
+      info={championshipEditionMock as Dados} 
+      championship={campeonato as unknown as Campeonato} 
+    />
   );
 }
 
